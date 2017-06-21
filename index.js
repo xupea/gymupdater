13b007521f491fb992e530668cfc9e594bb812b6
Peter Hsu <sysu.peter.hsu@gmail.com>
2017-06-21 16:07:25 +0800

    prevent stop

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var http = require('http');
+var moment= require('moment');
+
+moment.locale('zh-cn');
+
 var wilddog = require('wilddog');
 var config = {
   authDomain: "gympro2017.wilddog.com",
@@ -8,6 +13,10 @@ wilddog.initializeApp(config);
 var ref = wilddog.sync().ref('baoan-stadium-tennis-court1/today');
 
 var available = false;
+
+setInterval(function() {
+  http.get('http://www.google.com');
+}, 300000);
 
 setInterval(function() {
   available = !available;
