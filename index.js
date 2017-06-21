174e3196f93aa72d2f8d7fb47587e860b7d8debf
Peter Hsu <sysu.peter.hsu@gmail.com>
2017-06-21 16:57:20 +0800

    add express

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var http = require('http');
 var moment= require('moment');
 
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 moment.locale('zh-cn');
 
 var wilddog = require('wilddog');
@@ -15,7 +34,7 @@ var ref = wilddog.sync().ref('baoan-stadium-tennis-court1/today');
 var available = false;
 
 setInterval(function() {
-  http.get('http://www.google.com');
+  http.get('https://gymupdater.herokuapp.com/');
 }, 300000);
 
 setInterval(function() {
